2ef752ce1b8058b1cc31213920c80d7639f33206
Andrew Aquino <dawneraq@gmail.com>
2023-09-13 12:35:00 -0700

    add radio input for workout day

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,63 @@
+'use client';
+
+import { ChangeEvent, useState } from 'react';
+
+const workoutDays = ['push', 'pull', 'leg'] as const;
+type WorkoutDay = (typeof workoutDays)[number];
+
 export default function Home() {
+	const [workoutDay, setWorkoutDay] = useState<WorkoutDay>('push');
+
+	function isWorkoutDayChecked(value: string): boolean {
+		return workoutDay === value;
+	}
+
+	function handleWorkoutDayChange(event: ChangeEvent) {
+		const { target } = event;
+		setWorkoutDay((target as HTMLInputElement).value as WorkoutDay);
+	}
+
 	return (
-		<main>
-			<h1>hello world</h1>
-		</main>
+		<>
+			<header>
+				<h1>531</h1>
+				<fieldset>
+					<legend>choose workout</legend>
+					<div>
+						<label>
+							<input
+								type="radio"
+								name="workout-day"
+								value="push"
+								checked={isWorkoutDayChecked('push')}
+								onChange={handleWorkoutDayChange}
+							/>
+							push
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="workout-day"
+								value="pull"
+								checked={isWorkoutDayChecked('pull')}
+								onChange={handleWorkoutDayChange}
+							/>
+							pull
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="workout-day"
+								value="leg"
+								checked={isWorkoutDayChecked('leg')}
+								onChange={handleWorkoutDayChange}
+							/>
+							leg
+						</label>
+					</div>
+				</fieldset>
+			</header>
+			<main>TODO</main>
+		</>
 	);
 }
